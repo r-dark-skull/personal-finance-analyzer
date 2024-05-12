06bd52dffaabb04b8d2d3f37f3059df98248310f
krdark <krdarkskull>
2024-05-13 02:27:37 +0530

    compose setup

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-docker container rm mera_financial_analyst
+# docker container rm mera_financial_analyst
 # docker image rm personal-finance-analyzer:beta
 
-docker image build -t personal-finance-analyzer:beta .
-docker run  --name mera_financial_analyst -d -p 8000:8000 personal-finance-analyzer:beta
+# docker image build -t personal-finance-analyzer:beta .
+# docker run  --name mera_financial_analyst -d -p 8000:8000 personal-finance-analyzer:beta
 
+docker-compose up -d
 docker image prune
