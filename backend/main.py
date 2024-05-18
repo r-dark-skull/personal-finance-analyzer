4ea5270cf9e44daf0eae761309911357e4b09bdd
krdark <krdarkskull>
2024-05-18 16:41:22 +0530

    analysis update

--- backend/main.py
+++ backend/main.py
@@ -4,18 +4,3 @@ from router import create_app
 import server
 from core import GmailLoader
 app = create_app()
-
-
-# loader = GmailLoader()
-
-
-# async def get_resulls():
-#     mails = await loader.load_emails(
-#         email="garg.arnav77@gmail.com",
-#         filter_date="2024-05-15"
-#     )
-
-#     json.dump(mails, open("mails.json", "w"))
-
-
-# asyncio.run(get_resulls())
