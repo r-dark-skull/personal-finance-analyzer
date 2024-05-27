c8538f2e147045833ebf0213ac13addc8aac4cb7
krdark <krdarkskull>
2024-05-27 20:52:39 +0530

    email analysis bug fixes

--- backend/core/email_loader.py
+++ backend/core/email_loader.py
@@ -5,6 +5,12 @@ import server
 from base64 import b64decode
 from bs4 import BeautifulSoup
 from models import RawEmail
+from pydantic import ValidationError
+from hashlib import sha256
+
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 
 class GmailLoader:
@@ -35,59 +41,112 @@ class GmailLoader:
                 # re.sub(r'([a-z])\1+', r'\1', text_data)
 
             return text_data
-
-    async def __email_parser(self, mail: dict):
-
-        extracted_data = {}
-        body = ""
-
-        headers: list = mail.get('payload').get('headers')
-        payload_body: dict = mail.get('payload').get('body')
-        payload_parts: list = mail.get('payload').get('parts')
-
-        # extracting metadata
-        if headers:
-            for header in headers:
-                if header.get('name') in ["Date", "From"]:
-                    extracted_data[header["name"].lower()] = header["value"]
-                elif header.get('name') == 'Subject':
-                    body += f"Subject : {header['value']}\n\nContent: "
-
-        if not any(financial_service in extracted_data['from']
-                   for financial_service in server.credentials.get('info').get('financial_services')
-                   ):
+        else:
+            logger.warning(f"MIME TYPE not in html/text : {mime}")
+            logger.debug(f"{part}")
             return None
 
-        if payload_parts:
-            for part in payload_parts:
-                body += self.__gmail_part_parser(part)
+    def __create_mail_id(self, mail_body: str, date: str, sender: str) -> str:
+        text = f"MAIL BODY: {mail_body}\nOnDate: {date}\nSender: {sender}"
+        return sha256(text.encode()).hexdigest()
 
-            extracted_data["body"] = body
+    async def __email_parser(self, mail: dict, mail_id: str):
+        extracted_data = {}
+        body = ""
+        try:
+            headers: list = mail.get('payload').get('headers')
+            payload_body: dict = mail.get('payload').get('body')
+            payload_parts: list = mail.get('payload').get('parts')
 
-        return RawEmail(**extracted_data)
+            # extracting metadata
+            if headers:
+                for header in headers:
+                    if header.get('name') in ["Date", "From"]:
+                        extracted_data[header["name"].lower()
+                                       ] = header["value"]
+                    elif header.get('name') == 'Subject':
+                        body += f"Subject : {header['value']}\n\nContent: "
+
+            if not any(financial_service in extracted_data['from']
+                       for financial_service in server.credentials.get('info').get('financial_services')
+                       ):
+                return None
+
+            if payload_parts:
+                for part in payload_parts:
+                    body += self.__gmail_part_parser(part) or ""
+
+                extracted_data["body"] = body
+
+                try:
+
+                    mid = self.__create_mail_id(
+                        mail_body=extracted_data['body'],
+                        date=extracted_data['date'],
+                        sender=extracted_data['from']
+                    )
+
+                    return RawEmail(_id=mid, **extracted_data)
+                except ValidationError as ve:
+                    logger.error(
+                        f"Validation Error in Raw Email for MAIL ID - [{mail_id}] : {ve}")
+                    with open("/develop/cache/validation_errors.log", "a") as ve_file:
+                        ve_file.write(f"{mail_id}\n")
+        except Exception as e:
+            logger.error(
+                f"Error in Email Parser : {e}"
+            )
+            raise e
 
     async def __email_getter(self, mail_id: str):
-        mail = self.__service.users().messages().get(
-            userId=self.__user_id, id=mail_id
-        ).execute()
 
-        return await self.__email_parser(mail)
+        try:
+            mail = self.__service.users().messages().get(
+                userId=self.__user_id, id=mail_id
+            ).execute()
+        except Exception as e:
+            logger.error(f"Error in Email Getter : {e}")
+            raise e
 
-    async def load_emails(self, email: str, filter_date: str):
-        mail_list = self.__service.users().messages().list(
-            userId=self.__user_id,
-            q=f"after:{filter_date} "
-        ).execute()
+        return await self.__email_parser(mail, mail_id)
+
+    async def load_emails(self, start_date: str, end_date: str = None):
+
+        mail_list = []
+        nextPageToken = ""
+
+        filter_string = f"after:{start_date} before:{end_date}" \
+            if end_date else f"after: {start_date}"
+
+        while nextPageToken is not None:
+            resp = self.__service.users().messages().list(
+                userId=self.__user_id,
+                maxResults=500,
+                pageToken=nextPageToken,
+                q=filter_string
+            ).execute()
+
+            mail_list += resp.get('messages')
+            nextPageToken = resp.get('nextPageToken')
+            # nextPageToken = None
+
+        logger.debug(f"Total Number of Mails : {len(mail_list)}")
+        logger.debug(f"Mail Ids: {mail_list}")
 
         # creating task group
         futures = []
-
-        async with asyncio.TaskGroup() as mail_task_group:
-            for mail in mail_list.get('messages'):
-                futures.append(
-                    mail_task_group.create_task(
-                        self.__email_getter(mail.get('id'))
+        try:
+            async with asyncio.TaskGroup() as mail_task_group:
+                for mail in mail_list:
+                    futures.append(
+                        mail_task_group.create_task(
+                            self.__email_getter(mail.get('id'))
+                        )
                     )
-                )
+        except Exception as e:
+            logger.error(
+                f"Error in Loading Emails : {e}"
+            )
+            raise e
 
         return [task.result() for task in futures if task.result() is not None]
--- backend/core/email_manager.py
+++ backend/core/email_manager.py
@@ -3,6 +3,7 @@ import os
 from fastapi.encoders import jsonable_encoder
 from datetime import datetime
 from .email_loader import GmailLoader
+from pymongo.errors import DuplicateKeyError
 import server
 from typing import List
 from models import RawEmail, Transaction
@@ -21,22 +22,29 @@ class EmailManager:
     async def fetch_and_save_emails(self, start_date: str, end_date: str = None):
         try:
             emails: List[RawEmail] = await self.__loader.load_emails(
-                email=server.credentials.get('info').get('client_email'),
-                filter_date=start_date
+                start_date=start_date,
+                end_date=end_date
             )
+
+            logger.debug(f"Total Number of Emails to fetch : {len(emails)}")
 
             for mail in emails:
 
                 # recheck : must be always true
-                if any(financial_service in mail.sender
-                        for financial_service in server.credentials.get('info').get('financial_services')
-                       ):
+                if any(
+                        financial_service in mail.sender
+                    for financial_service in
+                    server.credentials.get('info').get('financial_services')
+                ):
 
                     email = jsonable_encoder(mail)
 
-                    server.context['database'][
-                        os.getenv('MAIL_COLLECTION')
-                    ].insert_one(email)
+                    try:
+                        server.context['database'][
+                            os.getenv('MAIL_COLLECTION')
+                        ].insert_one(email)
+                    except DuplicateKeyError as exp:
+                        logger.error(exp)
             return True
         except Exception as e:
             logger.error(e)
@@ -67,21 +75,37 @@ class EmailManager:
         mails_to_analyze = await self.__un_analyzed_mails()
 
         for mail in mails_to_analyze:
-            js_response = json.loads(
-                self.ai_connect.get_analysis(mail.mail_body)
-            )
 
-            if len(js_response):
-
-                js_response['date'] = datetime.strptime(
-                    js_response['date'], "%Y-%m-%d"
+            try:
+                js_response = json.loads(
+                    self.ai_connect.get_analysis(mail.mail_body)
                 )
-                js_response['id'] = mail.id
 
-                # getting and saving category
-                # category = await self.email_analyzer(js_response)
+                if len(js_response):
 
-                trx = jsonable_encoder(Transaction(**js_response))
-                server.context['database'][
-                    os.getenv('TRANSACTION_COLLECTION')
-                ].insert_one(trx)
+                    js_response['date'] = datetime.strptime(
+                        js_response['date'], "%Y-%m-%d"
+                    )
+                    js_response['id'] = mail.id
+
+                    # getting and saving category
+                    # category = await self.email_analyzer(js_response)
+
+                    trx = jsonable_encoder(Transaction(**js_response))
+                    server.context['database'][
+                        os.getenv('TRANSACTION_COLLECTION')
+                    ].insert_one(trx)
+
+                server.context['database'][os.getenv('MAIL_COLLECTION')].update_one(
+                    {
+                        "_id": mail.id
+                    },
+                    {
+                        "$set": {
+                            "is_analyzed": True
+                        }
+                    }
+                )
+            except Exception as e:
+                logger.error(f"Error for Mail ID : {mail.id}")
+                logger.error(e)
